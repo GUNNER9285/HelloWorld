43f4c117a768c6bd15739e18377d620d4ea3db26
GUNNER9285 <gunner9285@gmail.com>
2016-07-29 16:25:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gunner/a001helloworld/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.gunner.a001helloworld;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t;
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t = (TextView)findViewById(R.id.textView);
+        t.setText("Hello");
+
+        b = (Button)findViewById(R.id.buttonDialog);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("What your name ?");
+                builder.setPositiveButton("GUN", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Thank you.",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+        Button b2 = (Button) findViewById(R.id.showToastButton);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Thank you.",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button b3 = (Button) findViewById(R.id.exitButton);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
